baebaf27476ca2fbf81a03f2c040cabeb816ed7f
alejolo311 <alejolo311@gmail.com>
2021-01-16 22:05:54 -0500

    static files dir

--- core/settings.py
+++ core/settings.py
@@ -126,8 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(
-    BASE_DIR), "static")
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
